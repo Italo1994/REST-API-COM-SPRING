7d7a2b6f9027abc0a1959c7ef3dec567b0b50374
Italo1994 <italohortiz2015@gmail.com>
2023-05-04 03:43:18 -0300

    finishing the mapping routes with the @DeleteMapping and @PostMapping annotations.

---
+++ src/main/java/dio/web/api/controller/UsuarioController.java
@@ -0,0 +1,43 @@
+package dio.web.api.controller;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import dio.web.api.model.Usuario;
+import dio.web.api.repository.UsuarioRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+// import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+// @RequestMapping("/users")
+public class UsuarioController {
+    @Autowired
+    private UsuarioRepository repository;
+    
+    @GetMapping("/users")
+    public List<Usuario> getUsers() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/users/{username}")
+    public Usuario getOne(@PathVariable("username") String username) {
+        return repository.findByUsername(username);
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void delete(@PathVariable("id") Integer id) {
+        repository.deleteById(id);
+    }
+
+    @PostMapping("/users")
+    public void postUser(@RequestBody Usuario usuario) {
+        repository.save(usuario);
+    }
+}
